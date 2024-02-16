295a4a837966800c5719abc8e602279a0e980d97
vadiimXD <vadim123bg@gmail.com>
2024-02-16 12:13:56 +0200

    Imported  basic skeletn

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+
+//here need to be controllers for routing 
+//EXAMPLE
+const firstController = require("./controller/test1Controller")
+const secondController = require("./controller/test2Controller")
+const thirdController = require("./controller/test3Controller")
+const authController = require("./controller/authController")
+
+
+//app use routes
+
+//EXAMPLE
+router.use(firstController)
+router.use(secondController)
+router.use(thirdController)
+
+
+//for other all
+router.all("*", (req, res) => {
+    res.send("404")
+})
+
+module.exports = router
